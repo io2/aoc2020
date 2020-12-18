80e428899caeb3bebc220e366461ea044f612a32
Otu Ekanem <o.ekanem@gmail.com>
2020-12-18 12:51:56 +0100

    aoc 2020 - d1

---
+++ README.md
@@ -0,0 +1,29 @@
+# Advent of Code 2020
+
+**Description**
+
+    input files are available at `inputs/$day_num.txt`
+    code is in `libs/D$day_num.ex`
+
+    to run in iex
+
+```sh
+    > iex -S mix
+    D1.run
+    D2.run
+
+    open D1.run    # to go directly to code from iex
+```
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `aoc2020` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:aoc2020, "~> 0.1.0"}
+  ]
+end
+```
---
+++ lib/D1.ex
@@ -0,0 +1,33 @@
+defmodule D1 do
+
+  def run do
+    "inputs/1.txt"
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.map(&(String.to_integer(&1)))
+    |> check
+  end
+
+  # check all
+  defp check(l) when is_list(l) do
+    case check(hd(l), tl(l)) do
+      nil -> check(tl(l))
+      found -> found
+    end
+  end
+
+  # check head against sublist
+  defp check(_h, []), do: nil
+  defp check(h, t) when is_list(t) do
+    case check(h, hd(t)) do
+      nil -> check(h, tl(t))
+      found -> found
+    end
+  end
+
+  # do maths
+  defp check(a, b) when a + b == 2020, do: a * b
+  defp check(a, b) when a + b != 2020, do: nil
+
+end
